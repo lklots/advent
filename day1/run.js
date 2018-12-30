5bae0f3e822c3f9893b55d936be53e6d3895075b
Levy Klots <levyklots@gmail.com>
2018-12-29 18:41:03 -0800

    [day1] run part 2 against input file

--- day1/run.js
+++ day1/run.js
@@ -5,6 +5,7 @@ const path = require('path');
 const util = require('util');
 
 const aggFreqs = require('./aggFreqs');
+const repeatFreqs = require('./repeatFreqs');
 
 const readFile = util.promisify(fs.readFile);
 
@@ -13,6 +14,11 @@ async function run() {
   const freqs = file.toString().split('\n');
   const total = aggFreqs(freqs);
   console.log(total);
+
+  const file2 = await readFile(path.join(__dirname, 'input2.txt'));
+  const freqs2 = file2.toString().split('\n');
+  const repeating = repeatFreqs(freqs2);
+  console.log(repeating);
 }
 
 run();
