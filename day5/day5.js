2ab1c67e1cff67a2d3c860c82a413125442a38e3
Levy Klots <levyklots@gmail.com>
2019-01-07 22:44:30 -0800

    [day5] part 2 quick and dirty

--- day5/day5.js
+++ day5/day5.js
@@ -6,24 +6,36 @@ const fs = require('fs');
 
 const readFile = util.promisify(fs.readFile);
 
-function combust(str) {
+function combustButIgnore(str, ignoreChars) {
   const stack = [];
   for (let i = 0; i < str.length; i += 1) {
-    if (!stack.length) {
-      stack.push(str[i]);
-    } else if (Math.abs(stack[stack.length - 1].charCodeAt(0) - str.charCodeAt(i)) === 32) {
-      stack.pop();
-    } else {
-      stack.push(str[i]);
+    if (!(ignoreChars.includes(str[i]))) {
+      if (!stack.length) {
+        stack.push(str[i]);
+      } else if (Math.abs(stack[stack.length - 1].charCodeAt(0) - str.charCodeAt(i)) === 32) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
     }
   }
 
-  console.log(stack.length);
+  return stack.length;
 }
 
 async function run() {
   const contents = await readFile(path.join(__dirname, 'input.txt'));
-  combust(contents.toString());
+  console.log(`comubsted squence length is: ${combustButIgnore(contents.toString(), [])}`);
+
+  const lengths = [];
+  for (let i = 65; i < 65 + 26; i += 1) {
+    const ignore = [String.fromCharCode(i), String.fromCharCode(i + 32)];
+    const length = combustButIgnore(contents.toString(), ignore);
+    console.log(`${ignore}: ${length}`);
+    lengths.push(length);
+  }
+
+  console.log(`min length: ${Math.min(...lengths)}`);
 }
 
 run();
