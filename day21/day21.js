6c34d4e2183c5e68384fd6adb71291509d747d8e
Levy Klots <levyklots@gmail.com>
2019-02-03 13:04:07 -0200

    [day21] failed attempt

---
+++ day21/day21.js
@@ -0,0 +1,56 @@
+#!/usr/local/bin/node
+// WRONG ANSWER
+const readFile = require('../lib/file');
+
+let REGISTERS = [0, 0, 0, 0, 0, 0];
+
+function register(reg) {
+  return REGISTERS[reg];
+}
+
+const OPCODES = {
+  addr: (a, b) => register(a) + register(b),
+  addi: (a, b) => register(a) + b,
+  mulr: (a, b) => register(a) * register(b),
+  muli: (a, b) => register(a) * b,
+  banr: (a, b) => register(a) & register(b),
+  bani: (a, b) => register(a) & b,
+  borr: (a, b) => register(a) | register(b),
+  bori: (a, b) => register(a) | b,
+  setr: (a, b) => register(a),
+  seti: (a, b) => a,
+  gtir: (a, b) => (a > register(b) ? 1 : 0),
+  gtri: (a, b) => (register(a) > b ? 1 : 0),
+  gtrr: (a, b) => (register(a) > register(b) ? 1 : 0),
+  eqir: (a, b) => (a === register(b) ? 1 : 0),
+  eqri: (a, b) => (register(a) === b ? 1 : 0),
+  eqrr: (a, b) => (register(a) === register(b) ? 1 : 0),
+};
+
+async function run() {
+  const lines = (await readFile(__dirname, 'input.txt')).split('\n');
+
+  let total = 0;
+  const ipregister = parseInt(lines.shift().match(/#ip (\d)/)[1], 10);
+  const instructions = lines.map(x => x.split(' ')).filter(x => x[0].length);
+  console.log(instructions);
+  for (let i = 0; i < instructions.length; i += 1) {
+    REGISTERS = [0, 0, 0, 0, 0];
+    REGISTERS[ipregister] = i;
+    while (REGISTERS[ipregister] < instructions.length && total < 1000000) {
+      let [opcode, a, b, c] = instructions[REGISTERS[ipregister]];
+      a = parseInt(a, 10);
+      b = parseInt(b, 10);
+      c = parseInt(c, 10);
+      const before = REGISTERS.slice();
+      REGISTERS[c] = OPCODES[opcode](a, b);
+      //console.log(`ip=${before[ipregister]} [${before.join(',')}] ${opcode} ${a} ${b} ${c} [${REGISTERS.join(',')}]`);
+      REGISTERS[ipregister] += 1;
+      total += 1;
+    } 
+    console.log(`ip(${i}): ${total}`);
+    total = 0;
+  }
+}
+
+run();
