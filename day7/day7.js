04155b7d9be41b83b33cf647e6bef429fd3a55ea
Levy Klots <levyklots@gmail.com>
2019-02-09 18:47:51 -0300

    [day7] solution to part 2

--- day7/day7.js
+++ day7/day7.js
@@ -39,10 +39,61 @@ class Graph {
   }
 }
 
-function topoSort(graph) {
-  let sorted = [];
+const MAX_WORK_Q = 5;
+
+function fillWork(work, workQ, queue) {
+  while (workQ.length < MAX_WORK_Q && queue.length) {
+    queue.sort().reverse();
+    const node = queue.pop();
+    if (work[node] > 0) {
+      if (workQ.length >= MAX_WORK_Q) {
+        queue.push(node);
+      } else {
+        workQ.push(node);
+      }
+    }
+  }
+}
+
+function topoSortAndWait(graph) {
+  const sorted = [];
   const degree = graph.getNodes().reduce((o, n) => Object.assign(o, { [n]: graph.inDegree(n) }), {});
-  const queue = _.keys(_.pickBy(degree, (v, k) => v === 0));
+  const work = _.zipObject(graph.getNodes(), graph.getNodes().map(char => char.charCodeAt(0) - 64 + 60));
+  console.log(work);
+  const queue = _.keys(_.pickBy(degree, v => v === 0));
+  queue.sort().reverse();
+  const workQ = queue.splice(0, Math.min(MAX_WORK_Q, queue.length));
+  let clock = 0;
+  do {
+    console.log(`${clock}:\t${workQ.join(',')}\t\t${sorted.join(',')}`);
+    fillWork(work, workQ, queue);
+    workQ.forEach((node) => {
+      work[node] -= 1;
+    });
+    workQ.forEach((node) => {
+      if (work[node] === 0) {
+        sorted.push(node);
+        graph.getDeps(node).forEach((dep) => {
+          degree[dep] -= 1;
+          if (degree[dep] === 0) {
+            queue.push(dep);
+          }
+        });
+        delete work[node];
+        workQ.splice(workQ.findIndex(c => c === node), 1);
+        fillWork(work, workQ, queue);
+      }
+    });
+    clock += 1;
+  } while (Object.keys(work).length);
+
+  return clock;
+}
+
+function topoSort(graph) {
+  const sorted = [];
+  const degree = graph.getNodes().reduce((o, n) => Object.assign(o, { [n]: graph.inDegree(n) }), {});
+  const queue = _.keys(_.pickBy(degree, v => v === 0));
   while (queue.length) {
     queue.sort().reverse();
     const node = queue.pop();
@@ -68,7 +119,9 @@ async function run() {
     }
   });
 
-  console.log(topoSort(graph).join(''));
+  console.log(`part 1: ${topoSort(graph).join('')}`);
+  console.log(`part 2: ${topoSortAndWait(graph)}`);
+
 }
 
 run();
