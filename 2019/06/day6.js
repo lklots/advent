beb76e8b38aedccba2d22db21c83a981afcf9328
Levy Klots <levyklots@gmail.com>
2019-12-11 17:00:52 -0800

    2019/06/part1

---
+++ 2019/06/day6.js
@@ -0,0 +1,39 @@
+const _ = require('lodash');
+const readInput = require('../../lib/file');
+
+
+function bfs(tree) {
+  let level = ['COM'];
+  let next = [];
+  let depth = 1;
+  let total = 0;
+  while (level.length) {
+    next = _.flatMap(level.map(n => tree.get(n) || []));
+    total += (depth * next.length);
+    level = next.slice();
+    depth += 1;
+  }
+  return total;
+}
+
+function add(tree, parent, child) {
+  if (!tree.get(parent)) {
+    tree.set(parent, []);
+  }
+  tree.get(parent).push(child);
+}
+
+async function run() {
+  const input = await readInput('2019/06/');
+  const orbits = _.map(input.split('\n'), x => x.split(')'));
+
+  const tree = new Map();
+  orbits.forEach(([parent, child]) => {
+    add(tree, parent, child);
+  });
+
+
+  console.log(bfs(tree));
+}
+
+run();
