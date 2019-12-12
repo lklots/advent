30dbdf0dda50e95db352f4b90987cd43b4a52c06
Levy Klots <levyklots@gmail.com>
2019-12-11 16:32:09 -0800

    2019/05/part2

--- 2019/05/day5.js
+++ 2019/05/day5.js
@@ -10,16 +10,22 @@ function decode(op) {
   return [code % 100, [digit(code, 2), digit(code, 3), digit(code, 4), digit(code, 5)]];
 }
 
-function exec(registers_) {
-  const inputs = [1];
+function exec(registers_, input) {
   const registers = registers_; // make linter happy
   let ip = 0;
-
   const OPS = {
     1: (x, y) => x + y,
     2: (x, y) => x * y,
-    3: (x) => { registers[x] = inputs.shift(); },
-    4: x => console.log(`val ${registers[x]}`),
+    3: (x) => { registers[x] = input; },
+    4: x => console.log(`val ${x}`),
+    5: (x, y) => {
+      ip = x !== 0 ? y : ip + 3;
+    },
+    6: (x, y) => {
+      ip = x === 0 ? y : ip + 3;
+    },
+    7: (x, y) => +(x < y),
+    8: (x, y) => +(x === y),
   };
 
   const resolve = (mode, index) => (mode ? registers[index] : registers[registers[index]]);
@@ -28,16 +34,30 @@ function exec(registers_) {
   while (opcode !== 99) {
     [opcode, modes] = decode(registers[ip]);
     const op = OPS[opcode];
+    if (opcode === 99) { break; }
     switch (opcode) {
       case 1:
       case 2:
+      case 7:
+      case 8:
+        // console.log(`${ip}: [${modes.join(',')}] ${registers[ip + 3]} = ${opcode}(${resolve(modes[0], ip + 1)}, ${resolve(modes[1], ip + 2)}):\t\t ${registers}`);
         registers[registers[ip + 3]] = op(resolve(modes[0], ip + 1), resolve(modes[1], ip + 2));
         ip += 4;
         break;
       case 3:
-      case 4:
+        // console.log(`${ip}: ${opcode}(${registers[ip + 1]}):\t\t ${registers}`);
         op(registers[ip + 1]);
         ip += 2;
+        break;
+      case 4:
+        // console.log(`${ip}: ${opcode}(${registers[ip + 1]}):\t\t ${registers}`);
+        op(resolve(modes[0], ip + 1));
+        ip += 2;
+        break;
+      case 5:
+      case 6:
+        // console.log(`${ip}: ip = ${opcode}(${resolve(modes[0], ip + 1)}, ${resolve(modes[1], ip + 2)}):\t\t ${registers}`);
+        op(resolve(modes[0], ip + 1), resolve(modes[1], ip + 2));
         break;
       case 99:
         break;
@@ -52,7 +72,7 @@ function exec(registers_) {
 async function run() {
   const input = await readInput('2019/05/');
   const registers = _.flatMap(input.split('\n'), x => x.split(',').map(y => parseInt(y, 10)));
-  exec(_.clone(registers), 12, 2);
+  exec(_.clone(registers), parseInt(process.argv[2], 10));
 }
 
 run();
