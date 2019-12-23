79433955955198ca6806fc05e5ec2ad725ef2e02
Levy Klots <levyklots@gmail.com>
2019-12-23 02:18:57 -0800

    2019/16/part2

--- 2019/16/day16.js
+++ 2019/16/day16.js
@@ -24,19 +24,39 @@ function take(gen, n) {
   return ret;
 }
 
+function drop(gen, n) {
+  _.times(n, () => gen.next());
+  return gen;
+}
+
 function round(seq) {
   const ret = seq.map((_d, i, arr) => _.zipWith(arr, take(pattern(i), arr.length), (x, y) => x * y));
   return ret.map(x => Math.abs(_.sum(x)) % 10);
 }
 
+function smartround(seq) {
+  for (let i = seq.length - 2; i >= 0; i -= 1) {
+    seq[i] = (seq[i + 1] + seq[i]) % 10;
+  }
+  return seq;
+}
+
 async function run() {
   const input = await readInput('2019/16/');
   let seq = input.split('').map(x => parseInt(x, 10));
-
   _.times(100, () => {
     seq = round(seq);
   });
-  console.log(_.take(seq, 8).join(''));
+  console.log(`${seq.slice(0, 8).join('')}`);
+
+  seq = input.split('').map(x => parseInt(x, 10));
+  const index = parseInt(seq.slice(0, 7).join(''), 10);
+  const repeat = 10000;
+  seq = take(drop(cycle(seq), index), seq.length * repeat - index);
+  _.times(100, () => {
+    seq = smartround(seq);
+  });
+  console.log(seq.slice(0, 8).join(''));
 }
 
 run();
