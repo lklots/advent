258e8cd1ee8b0323988976c78bfd11c57b101838
Levy Klots <levyklots@gmail.com>
2019-12-28 16:32:26 -0800

    2019/22/progress part 2

--- 2019/22/day22.js
+++ 2019/22/day22.js
@@ -1,47 +1,93 @@
 const _ = require('lodash');
 const readInput = require('../../lib/file');
 
-let deck = _.range(10007);
-function cut(num) {
-  if (num >= 0) {
-    const removed = deck.splice(0, num);
-    deck = deck.concat(removed);
-  } else {
-    const removed = deck.splice(deck.length + num, Math.abs(num));
-    deck = removed.concat(deck);
-  }
-  return deck;
+function mod(n, m) {
+  return ((n % m) + m) % m;
 }
 
-function increment(num) {
+function dealir(length, i) {
+  return (length - 1 - i) % length;
+}
+
+function cutir(length, i, n) {
+  return mod(i + n, length);
+}
+
+function incrementir(length, i, n) {
+  console.log(`${i},${n},${length} => ${(i * (length - n + 1)) % length}`);
+  return (i * (length - n)) % length;
+}
+
+
+function cut(deck, num) {
+  if (num >= 0) {
+    const removed = deck.splice(0, num);
+    return deck.concat(removed);
+  }
+  const removed = deck.splice(deck.length + num, Math.abs(num));
+  return removed.concat(deck);
+}
+
+function increment(deck, num) {
   const newDeck = deck.slice();
   deck.forEach((v, i) => {
     newDeck[(i * num) % deck.length] = v;
   });
-  deck = newDeck;
-  return deck;
+  return newDeck;
 }
 
-function deal() {
+function deal(deck) {
   deck.reverse();
   return deck;
 }
 
 async function run() {
+  // const DECK = 10007;
+  const DECK = 10;
   const input = await readInput('2019/22/');
   const instructions = input.split('\n').map((line) => {
     if (line.match(/cut/)) {
-      return () => cut(parseInt(line.match(/(-?[0-9]+)/)[1], 10));
+      return d => cut(d, parseInt(line.match(/(-?[0-9]+)/)[1], 10));
     }
     if (line.match(/increment/)) {
-      return () => increment(parseInt(line.match(/([0-9]+)/)[1], 10));
+      return d => increment(d, parseInt(line.match(/([0-9]+)/)[1], 10));
     }
     if (line.match(/new/)) {
-      return () => deal();
+      return d => deal(d);
     }
   });
-  instructions.map(fn => fn());
-  console.log(_.findIndex(deck, x => x === 2019));
+  let newdeck = _.range(11);
+
+  console.log(newdeck);
+  newdeck = increment(newdeck, 5);
+  console.log(newdeck);
+  console.log(increment(newdeck, ));
+  console.log(_.isEqual(_.range(10), increment(newdeck, 3)));
+  console.log('END');
+
+  let deck = _.range(DECK);
+  deck = instructions.reduce((d, fn) => fn(d), deck);
+  console.log(deck);
+  console.log(`part1: ${_.findIndex(deck, x => x === 2019)}`);
+
+  const instructions2 = input.split('\n').map((line) => {
+    if (line.match(/cut/)) {
+      return (l, i) => cutir(l, i, parseInt(line.match(/(-?[0-9]+)/)[1], 10));
+    }
+    if (line.match(/increment/)) {
+      return (l, i) => incrementir(l, i, parseInt(line.match(/([0-9]+)/)[1], 10));
+    }
+    if (line.match(/new/)) {
+      return (l, i) => dealir(l, i);
+    }
+  });
+  instructions2.reverse();
+  let ret = 1;
+  //for (let i = 0; i < 101741582076661; i += 1) {
+  ret = instructions2.reduce((i, fn) => fn(DECK, i), ret);
+  //}
+  console.log(ret);
+  console.log(_.range(DECK).map((fromIndex) => instructions2.reduce((i, fn) => fn(DECK, i), fromIndex)));
 }
 
 run();
